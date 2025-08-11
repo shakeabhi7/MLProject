2722a4afd999a5db3b699aaddf4623c3447e3f00
shakeabhi7 <kumarabhishekt7@gmail.com>
2025-08-11 21:29:39 +0530

    New Logger file

--- src/logger.py
+++ src/logger.py
@@ -2,11 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FOLDER = "logs"
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+DATE_FOLDER_NAME = datetime.now().strftime('%d_%m')
+
+# full folder path logs/dd_mm
+LOG_DIR = os.path.join(LOG_FOLDER,DATE_FOLDER_NAME)
+os.makedirs(LOG_DIR,exist_ok=True)
+
+#log file with full datetime name
+LOG_FILE_NAME = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR,LOG_FILE_NAME)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
